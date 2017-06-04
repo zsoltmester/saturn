65f3ea49295530929f93fc9800bb1cd88792a15d
Mester, Zsolt <zsolt.mester@misys.com>
2017-06-04 23:23:41 +0200

    Add name text field to the add table view.

---
+++ Saturn/Saturn/NameTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NameTableViewCell.swift
+//  Saturn
+//
+//  Created by Zsolt Mester on 2017. 06. 04..
+//  Copyright © 2017. Zsolt Mester. All rights reserved.
+//
+
+import UIKit
+
+class NameTableViewCell: UITableViewCell {
+
+	// MARK: - Properties
+
+	static let reuseIdentifier = "Name Cell"
+
+}
