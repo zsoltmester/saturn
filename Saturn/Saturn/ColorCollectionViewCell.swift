ad3ac6173856c8e3b1f1856fa088424b7f8f5b22
Mester, Zsolt <zsolt.mester@misys.com>
2017-06-05 03:27:33 +0200

    Create the color collection view controller.

---
+++ Saturn/Saturn/ColorCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ColorCollectionViewCell.swift
+//  Saturn
+//
+//  Created by Zsolt Mester on 2017. 06. 05..
+//  Copyright © 2017. Zsolt Mester. All rights reserved.
+//
+
+import Pastel
+import UIKit
+
+class ColorCollectionViewCell: UICollectionViewCell {
+
+	// MARK: - Properties
+
+	static let reuseIdentifier = "Color Cell"
+    
+	@IBOutlet weak var colorPastelView: PastelView! {
+
+		didSet {
+			colorPastelView.layer.cornerRadius = 10.0
+			colorPastelView.layer.masksToBounds = true
+
+			colorPastelView.startPastelPoint = .top
+			colorPastelView.endPastelPoint = .bottom
+
+			colorPastelView.animationDuration = 2;
+		}
+	}
+}
